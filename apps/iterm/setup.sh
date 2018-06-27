55d7013efda36c44ca33af86d4011f50f981e9d8
Gustavo Madeira Santana <gumadeiras@gmail.com>
2018-06-27 17:23:52 -0300

    updated readme + sh $home

--- apps/iterm/setup.sh
+++ apps/iterm/setup.sh
@@ -5,7 +5,7 @@ echo "[exec] setting up iterm"
 
 cp ./apps/iterm/Meslo\ LG\ M\ Regular\ for\ Powerline.ttf /Library/Fonts/
 
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "~/Documents/research/repos/dotfiles/apps/iterm/"
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/dotfiles/apps/iterm/"
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -int 1
 
 # echo "[exec] link iterm profile dir"
