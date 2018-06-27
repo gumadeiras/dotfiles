ce2d34d05fdda11a57e42ba873c5f74762f41264
Gustavo Madeira Santana <gumadeiras@gmail.com>
2018-06-27 17:38:42 -0300

    check for homebrew before installing

--- install.sh
+++ install.sh
@@ -5,7 +5,20 @@ xcode-select --install
 # http://tech.lauritz.me/caps-lock-as-control-escape/
 # install first: homebrew
 echo "installing brew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "Installing Homebrew"
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+fi
 
 echo "installing oh-my-zsh"
 # install oh-my-zsh
