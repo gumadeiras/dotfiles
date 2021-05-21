544dbd96a2a57e16d689736afcda5bb02052c14f
Gustavo Madeira Santana <gumadeiras@users.noreply.github.com>
2021-05-21 04:07:53 -0400

    added raycast

---
+++ apps/raycast/utils/say-google.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# @raycast.title sayg
+# @raycast.author Gustavo Santana
+# @raycast.authorURL https://github.com/gumadeiras
+# @raycast.description Pronounce a word
+# @raycast.schemaVersion 1
+
+# @raycast.icon 📣
+# @raycast.mode silent
+# @raycast.packageName Pronounce
+# @raycast.argument1 { "type": "text", "placeholder": "word" }
+
+cd $TMPDIR
+curl -o $1.mp3 "https://ssl.gstatic.com/dictionary/static/sounds/oxford/$1--_us_1.mp3"
+afplay $1.mp3
+rm $1.mp3
