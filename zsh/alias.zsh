c7aca1157a490232ab924d62a49d8f98bc9d1cd0
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-02-18 13:58:37 -0500

    Dotfiles: sync git credential helper and shell aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,2 +1,9 @@
 # Shell aliases
 alias conda="micromamba"
+
+# Quick utilities
+alias ll="ls -la"
+alias h="history"
+alias xc="clear"
+alias disk="df -h"
+alias topmem="ps aux --sort=-%mem | head -n 10"
