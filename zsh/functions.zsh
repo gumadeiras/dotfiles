293276f1a86d81e1c443c06c7148d32be9565132
Gustavo Madeira Santana <gumadeiras@gmail.com>
2026-02-18 12:49:35 -0500

    Zsh: split config into alias/functions and add env template

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,49 +1,71 @@
-# ffd - cd to selected directory
-ffd() {
-  local dir
-  dir=$(find ${1:-*} -path '*/\.*'         \
-                     -prune                \
-                     -o -type d            \
-                     -print 2> /dev/null | \
-                     fzf +m -x             \
-  ) && cd "$dir"
-                     # fzf --query="$1" \
+# Source - https://stackoverflow.com/a/30899296
+# Posted by Jamie Treworgy, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-07, License - CC BY-SA 4.0
+r-delregion() {
+  if ((REGION_ACTIVE)) then
+     zle kill-region
+  else
+    local widget_name=$1
+    shift
+    zle $widget_name -- $@
+  fi
 }
 
-# ffda - including hidden directories
-ffda() {
-  local dir
-  dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m -x) && cd "$dir"
+r-deselect() {
+  ((REGION_ACTIVE = 0))
+  local widget_name=$1
+  shift
+  zle $widget_name -- $@
 }
 
-# ffh - repeat history
-ffh() {
-  eval $(history | fzf +s -x| sed 's/ *[0-9]* *//')
+r-select() {
+  ((REGION_ACTIVE)) || zle set-mark-command
+  local widget_name=$1
+  shift
+  zle $widget_name -- $@
 }
 
-# ffkill - kill process
-ffkill() {
-  ps -ef   | \
-    sed 1d | \
-    fzf -m | \
-    awk '{print $2}' | \
-    xargs kill -${1:-9}
+# keep emacs-style keybindings with shift-selection support
+bindkey -e
+for key     kcap   seq        mode   widget (
+    sleft   kLFT   $'\e[1;2D' select   backward-char
+    sright  kRIT   $'\e[1;2C' select   forward-char
+    sup     kri    $'\e[1;2A' select   up-line-or-history
+    sdown   kind   $'\e[1;2B' select   down-line-or-history
+
+    send    kEND   $'\E[1;2F' select   end-of-line
+    send2   x      $'\E[4;2~' select   end-of-line
+
+    shome   kHOM   $'\E[1;2H' select   beginning-of-line
+    shome2  x      $'\E[1;2~' select   beginning-of-line
+
+    left    kcub1  $'\EOD'    deselect backward-char
+    right   kcuf1  $'\EOC'    deselect forward-char
+
+    end     kend   $'\EOF'    deselect end-of-line
+    end2    x      $'\E4~'    deselect end-of-line
+
+    home    khome  $'\EOH'    deselect beginning-of-line
+    home2   x      $'\E1~'    deselect beginning-of-line
+
+    csleft  x      $'\E[1;6D' select   backward-word
+    csright x      $'\E[1;6C' select   forward-word
+    csend   x      $'\E[1;6F' select   end-of-line
+    cshome  x      $'\E[1;6H' select   beginning-of-line
+
+    cleft   x      $'\E[1;5D' deselect backward-word
+    cright  x      $'\E[1;5C' deselect forward-word
+
+    del     kdch1   $'\E[3~'  delregion delete-char
+    bs      x       $'^?'     delregion backward-delete-char
+
+  ) {
+  eval "key-$key() {
+    r-$mode $widget \$@
+  }"
+  zle -N key-$key
+  bindkey ${terminfo[$kcap]-$seq} key-$key
 }
 
-# ffbr - checkout git branch
-ffbr() {
-  local branches branch
-  branches=$(git branch) \
-    && branch=$(echo "$branches" | fzf +s +m -x) \
-    && git checkout $(echo "$branch" | sed "s/.* //")
-}
-
-# ffco - checkout git commit
-ffco() {
-  local commits commit
-  commits=$(git log --pretty=oneline \
-                    --abbrev-commit  \
-                    --reverse)       \
-    && commit=$(echo "$commits" | fzf +s +m -e) \
-    && git checkout $(echo "$commit" | sed "s/ .*//")
-}
+# restore backward-delete-char for Backspace in the incremental search keymap
+bindkey -M isearch '^?' backward-delete-char
